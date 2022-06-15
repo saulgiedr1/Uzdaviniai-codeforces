e4ca50dc9e294ca8050337852630eb17c4a50373
saulgiedr1 <saulgiedr@gmail.com>
2022-06-15 10:45:38 +0300

    plyteles nebaigtas

--- PU3-plyteles/script.js
+++ PU3-plyteles/script.js
@@ -4,9 +4,9 @@ window.onload=function(){
     let plotis;
     let plytele;
 
-    ilgis=8;
-    plotis=4;
-    plytele=2;
+    ilgis=17;
+    plotis=9;
+    plytele=4;
     let kiekis;
 
     kiekis=plyteliuSkaicius(ilgis,plotis,plytele);
@@ -20,9 +20,15 @@ function plyteliuSkaicius(a,b,c){
   if ((a*b) % (Math.pow(c ,2))  === 0){
     return  a*b/Math.pow(c, 2);
   }
-  
-  
-
+   else if (a % c !== 0){
+    return  ((a-(a % c)) *b/Math.pow(c,2))+b/c ;    
+   }
+  else if (b % c !== 0){
+  return ((b-(b % c)) *a/Math.pow(c,2))+a/c ;
+}
+  else {
+   return   false;
+  } 
 
 
 }
